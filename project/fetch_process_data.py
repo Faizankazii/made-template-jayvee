50cf05400b98558fd5c5f4cf86ebffa80896e7af
Faizan <faizankadzi@gmail.com>
2024-12-05 03:54:56 +0100

    Optimize codebase

--- project/fetch_process_data.py
+++ project/fetch_process_data.py
@@ -1,64 +1,87 @@
-import os
-import requests
-import zipfile
 import pandas as pd
-from io import BytesIO
 import click
 
-def download_and_extract(url, extract_to):
-    click.secho(f"Downloading {url}...", bg='magenta')
-    response = requests.get(url)
-    if response.status_code == 200:
-        with zipfile.ZipFile(BytesIO(response.content)) as z:
-            z.extractall(extract_to)
-        click.secho(f"Extracted contents of {url} to {extract_to}.", bg='green')
-    else:
-        click.secho(f"Failed to download {url} with status code {response.status_code}.", bg='red')
-        exit(1)
+class MakeData():
+    def __init__(self) -> None:
+        self.airbnb_df = pd.read_csv('airbnb_data/AB_US_2023.csv', low_memory=False)
+        self.cities_df = pd.read_csv('uscities/uscities.csv', low_memory=False)
+        self.merged_df = pd.DataFrame()
 
-def transform_csv(file_path, file):
-    try:
-        click.secho(f"Processing {file_path}...", bg='magenta')
-        df = pd.read_csv(file_path, low_memory=False)
-        df.dropna(inplace=True)
-        df.to_csv(f"../data/cleaned_{file}", index=False)
-        os.remove(file_path)
-        click.secho(f"Transformed data saved to cleaned_{file}", bg='green')
-    except Exception as e:
-        click.secho(f"Error processing {file_path}: {e}", bg='red')
+    def clean_airbnb(self):
+        click.secho(f"Cleaning airbnb data", bg='magenta')
+        city_replacements = {
+        'Twin Cities MSA': 'Minneapolis',
+        'Washington D.C.': 'Washington',
+        'New York City': 'New York',
+        'Santa Clara County': 'Santa Clara',
+        'Santa Cruz County' : 'Santa Cruz',
+        'San Mateo County' : 'San Mateo',
+        'Broward County' : 'Florida city',
+        'Clark County' : 'Las Vegas'
+        }
+        try:
+            self.airbnb_df['city'] = self.airbnb_df['city'].replace(city_replacements)
+            self.airbnb_df = self.airbnb_df[["room_type", "price", "city"]]
+            self.airbnb_df = pd.get_dummies(self.airbnb_df, dtype='int', columns=['room_type'], prefix=['room_type'])
+            self.airbnb_df.dropna(inplace=True)
+            click.secho(f"Task successful", bg='green')
+        except Exception as e:
+            click.secho(f"Failed processing airbnb data due to {e}", bg='red')
 
-def merge_datas(df_paths):
-    if len(df_paths) == 2:
-        df_cities = pd.read_csv(df_paths[0])
-        df_airbnb = pd.read_csv(df_paths[1])
-        merged_df = pd.merge(df_airbnb, df_cities, how='inner', left_on='neighbourhood', right_on='city')
-        merged_df = merged_df.drop(columns=['neighbourhood', 'id_x', 'id_y'])
-        merged_df.to_csv('../data/merged_data.csv', index=False)
-    else:
-        click.secho(f"Cleaned data path length not two! Exiting...", bg='red')
-        exit(1)
+    def agg_airbnb(self):
+        click.secho(f"Aggregating airbnb data", bg='magenta')
+        self.airbnb_df = self.airbnb_df.groupby('city').agg(
+        price_mean=('price', 'mean'),
+        entire_home_apt_sum=('room_type_Entire home/apt', 'sum'),
+        hotel_room_sum=('room_type_Hotel room', 'sum'),
+        private_room_sum=('room_type_Private room', 'sum'),
+        shared_room_sum=('room_type_Shared room', 'sum'),
+        listing_count=('city', 'size')
+        )
+        self.airbnb_df = self.airbnb_df.reset_index(drop = False)
+        click.secho(f"Task successful", bg='green')
+
+    def clean_cities(self):
+        click.secho(f"Cleaning cities data", bg='magenta')
+        self.cities_df = self.cities_df[["population", "density", "city"]]
+        self.cities_df.dropna(inplace = True)
+        click.secho(f"Task successful", bg='green')
+
+    def agg_cities(self):
+        click.secho(f"Aggregating cities data", bg='magenta')
+        self.cities_df = self.cities_df.loc[self.cities_df.groupby('city')['population'].idxmax()]
+        self.cities_df = self.cities_df.reset_index(drop=True)
+        click.secho(f"Task successful", bg='green')
+
+    def merge_data(self):
+        click.secho(f"Merging datas...", bg='magenta')
+        try:
+            self.merged_df = pd.merge(
+                self.airbnb_df, 
+                self.cities_df, 
+                how='inner', 
+                on=['city']
+            )
+            self.merged_df.dropna(inplace=True)
+            click.secho(f"Task successful", bg='green')
+        except Exception as e:
+            click.secho(f"Failed in merging due to {e}", bg='red')
+
+    def get_merged_data(self):
+        return self.merged_df
+    
+    def process(self):
+        self.clean_airbnb()
+        self.agg_airbnb()
+        self.clean_cities()
+        self.agg_cities()
+        self.merge_data()
 
 def main():
-    data_links = [
-        "https://shorturl.at/JCGL2",
-        "https://simplemaps.com/static/data/us-cities/1.79/basic/simplemaps_uscities_basicv1.79.zip"
-    ]
-    data_dir = "../data"
-    clean_df_path = []
-    for url in data_links:
-        download_and_extract(url, data_dir)
-    for file in os.listdir(data_dir):
-        print(file) #uscities.csv
-        if file.endswith(".csv"):
-            file_path = os.path.join(data_dir, f"{file}")
-            print(file_path)   #../data/uscities.csv
-            clean_df_path.append(f"../data/cleaned_{file}")
-            transform_csv(file_path, file)
-        else:
-            file_path = os.path.join(data_dir, file)
-            os.remove(file_path)
-    if len(clean_df_path) == 2:
-        merge_datas(clean_df_path)
+    MAKE = MakeData()
+    MAKE.process()
+    data = MAKE.get_merged_data()
+    data.to_csv("Final_data.csv", index=False)
 
 if __name__ == "__main__":
     main()
