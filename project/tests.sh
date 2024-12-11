51c8afbbda592f7e387f4aa0b9eeed5e18ed773e
Faizan <faizankadzi@gmail.com>
2024-12-11 23:34:27 +0100

    Dead commit

--- project/tests.sh
+++ project/tests.sh
@@ -4,7 +4,9 @@ RED='\033[0;31m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
 
-echo -e "${YELLOW}Installing Python dependencies from requirements.txt....${NC}"
+ls
+
+echo -e "${YELLOW}Installing Python dependencies from requirements.txt.....${NC}"
 pip install -r requirements.txt
 
 echo "${YELLOW}Executing pipeline...${NC}"
